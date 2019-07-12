437edd5f191ed8edd63e274b810b082da93631cb
mark matthews <mark.matthews1@dwp.gov.uk>
2019-07-12 14:50:15 +0100

    Add tests for retriever

--- tests/test_retriever.py
+++ tests/test_retriever.py
@@ -1,3 +1,4 @@
+import os
 from acm_cert_retriever import retriever
 
 
@@ -16,6 +17,35 @@ def test_parse_args_for_retrieve_cert_will_return_valid_args_when_given_correct_
         --log-level CRITICAL
     """
     result = retriever.parse_args(args)
+    assert result.acm_key_arn == "A"
+    assert result.acm_cert_arn == "B"
+    assert result.keystore_path == "C"
+    assert result.keystore_password == "D"
+    assert result.private_key_alias == "E"
+    assert result.private_key_password == "F"
+    assert result.truststore_path == "G"
+    assert result.truststore_password == "H"
+    assert result.truststore_aliases == "I"
+    assert result.truststore_certs == "J"
+    assert result.log_level == "CRITICAL"
+
+
+def test_parse_args_for_retrieve_cert_will_return_valid_args_when_given_valid_env_vars():
+
+    os.environ['RETRIEVER_ACM_KEY_ARN'] = "A"
+    os.environ['RETRIEVER_ACM_CERT_ARN'] = "B"
+    os.environ['RETRIEVER_KEYSTORE_PATH'] = "C"
+    os.environ['RETRIEVER_KEYSTORE_PASSWORD'] = "D"
+    os.environ['RETRIEVER_PRIVATE_KEY_ALIAS'] = "E"
+    os.environ['RETRIEVER_PRIVATE_KEY_PASSWORD'] = "F"
+    os.environ['RETRIEVER_TRUSTSTORE_PATH'] = "G"
+    os.environ['RETRIEVER_TRUSTSTORE_PASSWORD'] = "H"
+    os.environ['RETRIEVER_TRUSTSTORE_ALIASES'] = "I"
+    os.environ['RETRIEVER_TRUSTSTORE_CERTS'] = "J"
+    os.environ['RETRIEVER_LOG_LEVEL'] = "CRITICAL"
+
+    result = retriever.parse_args("")
+
     assert result.acm_key_arn == "A"
     assert result.acm_cert_arn == "B"
     assert result.keystore_path == "C"
