f0f71924cf5ddf3a5bdcfdeed971020d28fb9591
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-03-31 12:26:49 +0100

    Add to setup py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     },
     package_dir={"": "src"},
     packages=setuptools.find_packages("src"),
-    install_requires=["ConfigArgParse", "boto3", "pyjks", "pyopenssl", "pycryptodome"],
+    install_requires=["ConfigArgParse", "boto3", "pyjks", "pyopenssl", "pycryptodome", "retrying"],
     classifiers=[
         "Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
